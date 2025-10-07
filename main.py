0e56e38527f30d7800d5e7ed1294f8c9ad952d46
Cooookie2468 <138745682+Cooookie2468@users.noreply.github.com>
2025-10-08 11:54:30 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from math import *
 
+primes = []
+
+trying = 1
+while True:
+    testing = 1
+    prime = True
+    while trying < sqrt(testing):
+        if testing/trying != 0:
+            prime = False
+        trying += 1
+    if prime == True:
+        primes.append(testing)
+    trying += 1 
+    print(primes)
+    
