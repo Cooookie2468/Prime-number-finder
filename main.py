95dc26e0ed13bc99bb55764b88bb600948d30152
Cooookie2468 <138745682+Cooookie2468@users.noreply.github.com>
2025-10-08 12:20:09 +1300

    Update main.py
    SHORTER!!!

--- main.py
+++ main.py
@@ -3,20 +3,15 @@ from math import *
 primes = []
 notPrimes = {}
 
-testing = 2
-for i in range(500):
-    trying = 2
+for testing in range(2,500):
     prime = True
-    while trying <= sqrt(testing):
+    for trying in range(2,ceil(sqrt(testing))):
         if testing%trying == 0:
             prime = False
             notPrimes[trying].append(testing)
             break
-        trying += 1
     if prime == True:
         primes.append(testing)
         notPrimes[testing]=[]
-    testing += 1 
 print(primes)
 print(notPrimes)
-    
