5b7ab1a87b54fe1d63c58fcdbee9852135791ef8
Cooookie2468 <138745682+Cooookie2468@users.noreply.github.com>
2025-10-08 12:03:25 +1300

    Update main.py
    it works???

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from math import *
 primes = []
 
 testing = 2
-while True:
+for i in range(500):
     trying = 2
     prime = True
     while trying <= sqrt(testing):
-        if testing/trying != 0:
+        if testing%trying == 0:
             prime = False
+            #print(f'{testing} not prime with {trying}')
         trying += 1
     if prime == True:
         primes.append(testing)
     testing += 1 
-    print(primes)
-    
+print(primes)
