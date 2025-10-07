5c8faa9f9ad6dab11af06b0abd23b272b562368f
Cooookie2468 <138745682+Cooookie2468@users.noreply.github.com>
2025-10-08 12:10:56 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from math import *
 
 primes = []
+notPrimes = {}
 
 testing = 2
 for i in range(500):
@@ -9,9 +10,13 @@ for i in range(500):
     while trying <= sqrt(testing):
         if testing%trying == 0:
             prime = False
-            #print(f'{testing} not prime with {trying}')
+            notPrimes[trying].append(testing)
+            break
         trying += 1
     if prime == True:
         primes.append(testing)
+        notPrimes[testing]=[]
     testing += 1 
 print(primes)
+print(notPrimes)
+    
